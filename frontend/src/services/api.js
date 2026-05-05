4a5cf2ed0303786a3fadf1f0319526e8b00398e3
Ranjitha <ranjithagowda9801@gmail.com>
2026-05-05 16:37:43 +0530

    connected frontend to backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: '/api' })
+const API = axios.create({ 
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+})
 
 // Attach token to every request
 API.interceptors.request.use(cfg => {
